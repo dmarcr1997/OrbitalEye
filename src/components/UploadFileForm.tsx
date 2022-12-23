0cbd104c75d4aaffc2e0a6796deaf7314d117b7f
Damon Marc Rocha II <dmarcr1997@gmail.com>
2022-12-22 23:34:35 -0500

    Data form todos and updated background image styles

--- src/components/UploadFileForm.tsx
+++ src/components/UploadFileForm.tsx
@@ -3,22 +3,32 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import InputLabel from "@mui/material/InputLabel";
-import OutlinedInput from "@mui/material/OutlinedInput";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import { NEODataObject } from '../models/INEODataObject';
+
 const UploadFileForm = ({neos}: any) => {
     const [studiedBody, setStudiedBody ] = useState('');
-    const [file, setFile] = useState();
-
+    const [file, setFile] = useState<any>({});
+    const [uploading, setUploading] = useState(false);
     function handleChangeNeo(name: string) {
         setStudiedBody(name);
     }
 
-    function addFile(e: any) {
-        setFile(e.target.value);
+    async function addFile(e: any) {
+        setUploading(true);
+        const files = e.target.files;
+        try {
+            console.log(files[0]);
+            setFile(files[0]);
+            // const added = await client.add(files[0]); ipfs connection TODO
+            // setFile({ filename: files[0].name, hash: added.path });
+        } catch (error) {
+            console.log("Error uploading file: ", error);
+        }
+        setUploading(false);
     }
 
     function getSelectNEOs() {
@@ -26,6 +36,20 @@ const UploadFileForm = ({neos}: any) => {
             return neos.map((value: NEODataObject) => <MenuItem onClick={() => handleChangeNeo(value.name)}value={value.name}>{value.name}</MenuItem>)
         return <MenuItem value={undefined}>No NEO Selected</MenuItem>
     }
+
+    function sendFile() {
+        if(!uploading) console.log("Sending file...", file);
+        //TODO: Send file to ipfs
+        setStudiedBody('')
+        setFile({})
+    }
+
+    const checkData = () => {
+        const shouldUpload = uploading || !file || studiedBody === ''
+        console.log("DEBUGG2", shouldUpload)
+        return shouldUpload
+    }
+
     return (
         <Box component={Paper} sx={{border: '2px solid #27163c', mt: 2, }}>
             <Grid container>
@@ -46,10 +70,13 @@ const UploadFileForm = ({neos}: any) => {
                     </Select>
                 </Grid>
                 <Grid item xs={12}>
-                    <InputLabel sx={{textAlign: 'left', ml: 2}} id="demo-simple-select-label">PDF File</InputLabel>
+                    <InputLabel sx={{textAlign: 'left', ml: 2}} id="demo-simple-select-label">Data Zip File</InputLabel>
                     <Button variant="outlined" sx={{margin: 'auto', alignItems: 'center', mb: 5}}>
-                        <input style={{textAlign: 'center', marginLeft:'10%'}} onChange={addFile} type="file" accept='application/pdf'/>
+                        <input style={{textAlign: 'center' }} onChange={addFile} type="file" accept='.zip,.rar,.7zip'/>
                     </Button>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button onClick={sendFile} sx={{ mt: 2, mb: 5 }} disabled={checkData()} variant="contained">Get NEOs</Button>
                 </Grid>
             </Grid>
         </Box>
--- src/pages/Dashboard/Dashboard.styles.ts
+++ src/pages/Dashboard/Dashboard.styles.ts
@@ -1,5 +1,12 @@
 import { withTheme } from '@emotion/react';
 import background from '../../assets/images/DashboardImg2.jpg';
 
-export const dashboardContainer = { backgroundImage: `url(${background})`, backgroundSize: 'cover', height: '100vh', width: '100vw' };
+export const dashboardContainer = { 
+    backgroundImage: `url(${background})`, 
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    width: '100vw',
+    height: '180vh',
+};
 export const dashboardNEOTable = { color: 'white', backgroundColor: 'black', maxWidth: '100%', maxHeight: '100%', overflow: 'scrollbar', boxSizing: 'content-box' };
