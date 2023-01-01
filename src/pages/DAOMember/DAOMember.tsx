103c82fa474f0c8f0a3574bad2ec83736b035150
Damon Marc Rocha II <dmarcr1997@gmail.com>
2023-01-01 14:03:53 -0500

    Added Render of DAO members

--- src/pages/DAOMember/DAOMember.tsx
+++ src/pages/DAOMember/DAOMember.tsx
@@ -1,4 +1,4 @@
-import { Box, Paper } from '@mui/material';
+import { Box, Paper, TableContainer, Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { Container } from '@mui/system';
@@ -10,12 +10,72 @@ const DAOMember = () => {
 
     const editionDropAddress = "0xd18a96A539af150646f6E84fAEf0420FD2f1B3e3"
     const { contract: editionDrop } = useContract(editionDropAddress, "edition-drop");
-    // Hook to check if the user has our NFT
+    const { contract: token } = useContract('0xCe3fF8622fa3539F3Be49f9A9A7dD8A46bAAc662', 'token');
+
     const { data: nftBalance } = useNFTBalance(editionDrop, address, "0")
 
     const hasClaimedNFT = useMemo(() => {
         return nftBalance && nftBalance.gt(0)
     }, [nftBalance])
+
+    const [memberTokenAmounts, setMemberTokenAmounts] = useState([]);
+
+    const [memberAddresses, setMemberAddresses] = useState<any>([]);
+
+    const shortenAddress = (str: string) => {
+        return str.substring(0, 6) + '...' + str.substring(str.length - 4);
+    };
+
+    // This useEffect grabs all the addresses of our members holding our NFT.
+    useEffect(() => {
+        if (!hasClaimedNFT) {
+            return;
+        }
+        const getAllAddresses = async () => {
+            try {
+            const memberAddresses = await editionDrop?.history.getAllClaimerAddresses(
+                0,
+            );
+            setMemberAddresses(memberAddresses);
+            console.log('🚀 Members addresses', memberAddresses);
+            } catch (error) {
+            console.error('failed to get member list', error);
+            }
+        };
+        getAllAddresses();
+    }, [hasClaimedNFT, editionDrop?.history]);
+
+    // This useEffect grabs the # of token each member holds.
+    useEffect(() => {
+        if (!hasClaimedNFT) {
+            return;
+        }
+
+        const getAllBalances = async () => {
+            try {
+                const amounts: any = await token?.history.getAllHolderBalances();
+                setMemberTokenAmounts(amounts);
+            console.log('👜 Amounts', amounts);
+            } catch (error) {
+                console.error('failed to get member balances', error);
+            }
+        };
+        getAllBalances();
+    }, [hasClaimedNFT, token?.history]);
+
+    // Now, we combine the memberAddresses and memberTokenAmounts into a single array
+    const memberList = useMemo(() => {
+        return memberAddresses.map((address: any) => {
+            
+            const member: any = memberTokenAmounts?.find(({ holder }) => holder === address);
+
+            return {
+                address,
+                tokenAmount: member?.balance.displayValue || '0',
+            };
+        });
+    }, [memberAddresses, memberTokenAmounts]);
+
     const notAMember = () => {
         return(
             <>
@@ -43,13 +103,35 @@ const DAOMember = () => {
     const member = () => {
         return (
             <>
-                <Typography sx={{ mt: 4, mb: 2, ml: 2 }} variant="h6" component="div">🍪DAO Member Page</Typography>
+                <Typography sx={{ mt: 4, mb: 2, ml: 2 }} variant="h4" component="div">🍪DAO Member Page</Typography>
                 <Typography sx={{ mt: 4, mb: 2, ml: 2 }} variant="body1" component="div">Congratulations on being a member</Typography>
+                <Typography sx={{ mt: 4, mb: 2, ml: 2 }} variant="h6" component="div">Member List</Typography>
+                <TableContainer sx={{ maxHeight: 400, border: '1px solid #EAEAEA', mt: 4, width: '50%', ml: 2, mb: 2 }} component={Paper}>
+                    <Table stickyHeader aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>ADDRESS</TableCell>
+                                <TableCell align="right">TOKEN AMOUNT</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            { memberList.map((member: any) => (
+                                <TableRow
+                                key={`${member.address}`}
+                                sx={{ '&:last-child td, &:last-child th': { borderRight: '1px dotted #EAEAEA' } }}
+                                >
+                                    <TableCell align="left">{member.address}</TableCell>
+                                    <TableCell align="right">{member.tokenAmount}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </>
         )
     }
     return (
-        <Container>
+        <Container maxWidth={false}>
             <Box component={Paper} sx={{border: '2px solid #27163c', textAlign: 'left', mt: 2 }}>
                 {hasClaimedNFT ? member() : notAMember()}
             </Box>
