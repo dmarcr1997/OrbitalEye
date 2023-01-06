4c5f7d4ff21029aa63414ead7d846eb7686d3e45
Damon Marc Rocha II <dmarcr1997@gmail.com>
2023-01-06 17:22:25 -0500

    Added Filelist fetch from backend

--- src/components/FileList.tsx
+++ src/components/FileList.tsx
@@ -1,9 +1,22 @@
+import { Typography, Grid } from "@mui/material";
+import { useEffect, useState } from "react";
+
 const FileList = () => {
-    //Call backend to get list of file names and hashes
-    //use ipfs hook and file hashes to get all files
-    //Create list of files that show authors name, file, and asteroid 
-    //Bounty proposal button
-    return (<div>File List</div>)
+    //DONE:Call backend to get list of file names and hashes
+    //TODO: use ipfs hook and file hashes to get all files
+    //TODO: Create list of files that show authors name, file, and asteroid 
+    //TODO: Bounty proposal button
+    const [files, setFiles] = useState([]);
+    useEffect(() => {
+        async function getFiles() {
+            const response = await fetch('http://localhost:4000/files')
+            const fetchedFiles = await response.json();
+            setFiles(fetchedFiles);
+        }
+        getFiles();
+    }, [])
+    //@ts-ignore
+    return (<Grid container sx={{ ml: 2, alignItems: 'center'}} spacing={2}>{files.map((file) => <Grid item xs={12}><Typography variant="body2">{file.fileName} - - {file.ipfsHash}</Typography></Grid>)}</Grid>)
 };
 
 export default FileList;
