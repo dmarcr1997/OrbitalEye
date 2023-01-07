9400e0cd77da6e6343f7ea2496f96eb1f5b2b36e
Damon Marc Rocha II <dmarcr1997@gmail.com>
2023-01-06 23:19:27 -0500

    Changed file fetch to deployed banckend app

--- src/components/FileList.tsx
+++ src/components/FileList.tsx
@@ -3,13 +3,12 @@ import { useEffect, useState } from "react";
 
 const FileList = () => {
     //DONE:Call backend to get list of file names and hashes
-    //TODO: use ipfs hook and file hashes to get all files
-    //TODO: Create list of files that show authors name, file, and asteroid 
+    //TODO: Create list of files that show authors name, file/link, and asteroid 
     //TODO: Bounty proposal button
     const [files, setFiles] = useState([]);
     useEffect(() => {
         async function getFiles() {
-            const response = await fetch('http://localhost:4000/files')
+            const response = await fetch('https://orbital-eye-back-end.vercel.app/files')
             const fetchedFiles = await response.json();
             setFiles(fetchedFiles);
         }
