191f002e9b03d1a23b97713d660c6388b0385068
Damon Marc Rocha II <dmarcr1997@gmail.com>
2023-01-10 14:41:11 -0500

    File Download From IPFS

--- src/components/FileList.tsx
+++ src/components/FileList.tsx
@@ -1,12 +1,23 @@
 import { Typography, Grid, List, IconButton, ListItemText } from "@mui/material";
+import DownloadIcon from '@mui/icons-material/Download';
 import ListItem from "@mui/material/ListItem";
+import useIPFS from "../hooks/useIPFS";
 
 const FileList = ({ files }: any) => {
+
+    const downloadFile = (hash: string, name: string) => {
+        const file = useIPFS(hash, name);
+        return (
+            <a href={file} download={name}><DownloadIcon /></a>
+        )
+
+    } 
     return (
-    <Grid container sx={{ ml: 2, alignItems: 'center'}} spacing={2}>
-        <List dense={false}>
+    <Grid container sx={{ alignItems: "flex-start", ml: 1}} spacing={2}>
+        <List sx={{alignContent: "center", width: '95%'}} dense={false}>
             {files.map((file: any) => 
-                <ListItem key={file._id}>
+                <ListItem key={file._id} 
+                    secondaryAction={downloadFile(file.ipfsHash, file.fileName)}>
                 <ListItemText
                     primary={<>{file.fileName} -- {file.subject}</>}
                     secondary={<>{file.creator} ... ${file.bountyAmt}</>}
---
+++ src/hooks/useIPFS.ts
@@ -0,0 +1,6 @@
+const useIPFS = (hash: string, filename: string) => {
+    return `https://gateway.ipfscdn.io/ipfs/${hash}?filename=${filename}`
+};
+    
+export default useIPFS;
+    
